6cfff61c65023f29ff4653f8e591b0a33afd2cb0
Ethan Meanor <df>
2021-11-14 13:46:57 -0500

    Install FontAwesome icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../js/bootstrap.js'
+import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
