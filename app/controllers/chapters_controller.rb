6f155aff6e79d9c3dd6a7158ac55105eb312cadd
Ethan Meanor <df>
2021-11-15 16:03:31 -0500

    Fix links

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,7 +1,7 @@
 class ChaptersController < ApplicationController
 
   def show
-    book = Edition.find(params[:edition_id]).books.find_by(position: params[:book_position] || params[:position])
+    book = Book.find_by(edition_id: params[:edition_id], position: params[:book_position] || params[:position])
     @chapter = Chapter.includes(:chapter_texts).find_by(book_id: book.id, position: params[:position])
   end
 
